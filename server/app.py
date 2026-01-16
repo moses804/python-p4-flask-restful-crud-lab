2d45b95257334d84f61366f1eef66b20ec4b264a
Moses Maina <mainacalton57@gmail.com>
2026-01-17 02:10:20 +0300

    lab complete

--- server/app.py
+++ server/app.py
@@ -3,15 +3,20 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+import os
 
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+# Use absolute path to database in server directory
+db_path = os.path.join(os.path.dirname(__file__), 'plants.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
+# Point migrations to server/migrations directory
+migrations_dir = os.path.join(os.path.dirname(__file__), 'migrations')
+migrate = Migrate(app, db, directory=migrations_dir)
 db.init_app(app)
 
 api = Api(app)
@@ -46,6 +51,25 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        
+        for key, value in data.items():
+            setattr(plant, key, value)
+        
+        db.session.add(plant)
+        db.session.commit()
+        
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        
+        return make_response('', 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
